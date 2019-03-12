e7c75118120cea39783ff52c5f88346cb15f67af
maheshk691 <20313704+maheshk691@users.noreply.github.com>
2019-03-12 18:06:22 +0530

    Update user_get.py

--- user_get.py
+++ user_get.py
@@ -4,7 +4,7 @@ import json
 def active_users_get():
 	active_users = []
 	headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Basic Y3J1YWRtaW46Y3J1YWRtaW4='}
-	resp = requests.get('http://tiger.in.alcatel-lucent.com:8060/rest-service/users-v1',  headers=headers)
+	resp = requests.get('http://test.com/rest-service/users-v1',  headers=headers)
 	data=resp.json()
 	for i in data['userData']:
 		active_users.append(i['userName'])
